e016b84027beff818ed48d2c8e10383f03ebc769
villaclara <villaclara1231@gmail.com>
2025-05-05 12:31:53 +0300

    added check if mouse is over game screen in drawmanager

--- Assets/DrawManagerScript.cs
+++ Assets/DrawManagerScript.cs
@@ -1,41 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawManager : MonoBehaviour
 {
 
-    private Camera _cam;
-    [SerializeField] private Line _linePrefab;
-    [SerializeField] public GameObject _bridge;
-    [SerializeField] private GameObject _intersectionCollider;
+	private Camera _cam;
+	[SerializeField] private Line _linePrefab;
+	[SerializeField] public GameObject _bridge;
+	[SerializeField] private GameObject _intersectionCollider;
 
 
 
-    public const float RESOLUTION = 0.02f;
+	public const float RESOLUTION = 0.02f;
 
-    private Line _currentLine;
-    void Start()
-    {
-        _cam = Camera.main;
-    }
+	private Line _currentLine;
+	void Start()
+	{
+		_cam = Camera.main;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        Vector2 mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+	// Update is called once per frame
+	void Update()
+	{
+		// Check if mouse is not over the screen.
+		// xz chu norm tyt.
+		Vector3 mousePos1 = Input.mousePosition;
+		var isMouseOverScreen = mousePos1.x >= 0 && mousePos1.x <= Screen.width &&
+			   mousePos1.y >= 0 && mousePos1.y <= Screen.height;
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            _currentLine = Instantiate(_linePrefab, mousePos, Quaternion.identity);
-            _intersectionCollider.transform.position = mousePos;
-        }
+		if (!isMouseOverScreen)
+		{
+			return;
+		}
 
-        if (Input.GetMouseButton(0))
-        {
-            _currentLine.SetPosition(mousePos);
-            _intersectionCollider.transform.position = mousePos;
-        }
-    }
+		Vector2 mousePos = _cam.ScreenToWorldPoint(Input.mousePosition);
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			_currentLine = Instantiate(_linePrefab, mousePos, Quaternion.identity);
+			_intersectionCollider.transform.position = mousePos;
+		}
+
+		if (Input.GetMouseButton(0))
+		{
+			_currentLine.SetPosition(mousePos);
+			_intersectionCollider.transform.position = mousePos;
+		}
+	}
 }
