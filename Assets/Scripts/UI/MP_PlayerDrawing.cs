408287b6c5ccaf25860a5e0c5659b1de69751b6f
villaclara <villaclara1231@gmail.com>
2025-07-23 01:17:27 +0300

    fixed when starting sequential game. made event subs in mp_playerdrawing in networkspawn

--- Assets/Scripts/UI/MP_PlayerDrawing.cs
+++ Assets/Scripts/UI/MP_PlayerDrawing.cs
@@ -24,18 +24,57 @@ public class MP_PlayerDrawing : NetworkBehaviour
 
 	private void Start()
 	{
-		_playerDrawingChanged.OnValueChanged += OnValueChanged;
-		_isP1Turn.OnValueChanged += OnPlayerTurnChanged;
+		//_playerDrawingChanged.OnValueChanged += OnValueChanged;
+		//_isP1Turn.OnValueChanged += OnPlayerTurnChanged;
 
 
-		_p1Bridges.OnValueChanged += OnP1BridgesCountChanged;
-		_p2Bridges.OnValueChanged += OnP2BridgesCountChanged;
+		//_p1Bridges.OnValueChanged += OnP1BridgesCountChanged;
+		//_p2Bridges.OnValueChanged += OnP2BridgesCountChanged;
 	}
 
+	/// <inheritdoc/>
+	public override void OnNetworkSpawn()
+	{
+		if (IsServer)
+		{
+			PlayerManager.OnPlayerTurnSwitch += PlayerManager_OnPlayerTurnSwitch;
+			PlayerManager.OnPlayerBridgesChanged += PlayerManager_OnPlayerBridgesChanged;
+		}
+
+		_playerDrawingChanged.OnValueChanged += OnPlayerDrawingValueChanged;
+		_isP1Turn.OnValueChanged += OnPlayerTurnNetworkVariableChanged;
+		
+		_p1Bridges.OnValueChanged += OnP1BridgesCountChanged;
+		_p2Bridges.OnValueChanged += OnP2BridgesCountChanged;
+
+		base.OnNetworkSpawn();
+	}
+
+	/// <inheritdoc/>
+	public override void OnNetworkDespawn()
+	{
+		if (IsServer)
+		{
+			PlayerManager.OnPlayerTurnSwitch -= PlayerManager_OnPlayerTurnSwitch;
+			PlayerManager.OnPlayerBridgesChanged -= PlayerManager_OnPlayerBridgesChanged;
+		}
+
+		_playerDrawingChanged.OnValueChanged -= OnPlayerDrawingValueChanged;
+		_isP1Turn.OnValueChanged -= OnPlayerTurnNetworkVariableChanged;
+
+		_p1Bridges.OnValueChanged -= OnP1BridgesCountChanged;
+		_p2Bridges.OnValueChanged -= OnP2BridgesCountChanged;
+
+		base.OnNetworkDespawn();
+	}
+
+	/// <summary>
+	/// Handles the <see cref="_p2TextTMP"/> text in the UI with newest values.
+	/// </summary>
 	private void OnP2BridgesCountChanged(int previousValue, int newValue)
 	{
 		// Do not redraw text when the value has not changed. Not sure if it is actually needed.
-		if(previousValue == newValue)
+		if (previousValue == newValue)
 		{
 			return;
 		}
@@ -44,7 +83,9 @@ public class MP_PlayerDrawing : NetworkBehaviour
 		_p2TextTMP.gameObject.transform.parent.GetComponent<Animation>().Play("PlayerBridgesAnimation");
 	}
 
-
+	/// <summary>
+	/// Handles the <see cref="_p1TextTMP"/> text in the UI with newest values.
+	/// </summary>
 	private void OnP1BridgesCountChanged(int previousValue, int newValue)
 	{
 		// Do not redraw text when the value has not changed.
@@ -57,8 +98,13 @@ public class MP_PlayerDrawing : NetworkBehaviour
 		_p1TextTMP.gameObject.transform.parent.GetComponent<Animation>().Play("PlayerBridgesAnimation");
 	}
 
-	private void OnPlayerTurnChanged(bool previousValue, bool newValue)
+	/// <summary>
+	/// Handles the change of boolean <see cref="_isP1Turn"/> network variable 
+	/// that is updated when <see cref="PlayerManager.OnPlayerTurnSwitch"/> is invoked.
+	/// </summary>
+	private void OnPlayerTurnNetworkVariableChanged(bool previousValue, bool newValue)
 	{
+		Debug.Log($"Calling {nameof(OnPlayerTurnNetworkVariableChanged)}, with new valuue - {newValue}");
 		PlayerManager.playerTurn = newValue ? PlayerTurn.P1_Turn : PlayerTurn.P2_Turn;
 		// Changing the values in Clients to reflect actual value of player turn.
 		PlayerManager.player1.IsMyTurn = PlayerManager.playerTurn == PlayerTurn.P1_Turn;
@@ -66,48 +112,30 @@ public class MP_PlayerDrawing : NetworkBehaviour
 		OnPlayerTurnChangedInMPNetworkVar?.Invoke();
 	}
 
-	private void OnValueChanged(FixedString64Bytes previousValue, FixedString64Bytes newValue)
+	/// <summary>
+	/// Handles the UI text of <see cref="_playerDrawingTMP"/>, which indicates which player is currently making turn.
+	/// </summary>
+	private void OnPlayerDrawingValueChanged(FixedString64Bytes previousValue, FixedString64Bytes newValue)
 	{
 		_playerDrawingTMP.text = newValue.ToString();
 	}
 
-	public override void OnNetworkDespawn()
-	{
-		_playerDrawingChanged.OnValueChanged -= OnValueChanged;
-		_p1Bridges.OnValueChanged -= OnP1BridgesCountChanged;
-		_p2Bridges.OnValueChanged -= OnP2BridgesCountChanged;
-		_isP1Turn.OnValueChanged -= OnPlayerTurnChanged;
-		PlayerManager.OnPlayerTurnSwitch -= PlayerManager_OnPlayerTurnSwitch;
-		PlayerManager.OnPlayerBridgesChanged -= PlayerManager_OnPlayerBridgesChanged;
-		base.OnNetworkDespawn();
-	}
-
-	public override void OnDestroy()
-	{
-		_playerDrawingChanged.OnValueChanged -= OnValueChanged;
-		_p1Bridges.OnValueChanged -= OnP1BridgesCountChanged;
-		_p2Bridges.OnValueChanged -= OnP2BridgesCountChanged;
-		_isP1Turn.OnValueChanged -= OnPlayerTurnChanged;
-		PlayerManager.OnPlayerTurnSwitch -= PlayerManager_OnPlayerTurnSwitch;
-		PlayerManager.OnPlayerBridgesChanged -= PlayerManager_OnPlayerBridgesChanged;
-		base.OnDestroy();
-	}
-
-	public override void OnNetworkSpawn()
-	{
-		if(IsServer)
-		{
-			PlayerManager.OnPlayerTurnSwitch += PlayerManager_OnPlayerTurnSwitch;
-			PlayerManager.OnPlayerBridgesChanged += PlayerManager_OnPlayerBridgesChanged;
-		}
-	}
-
+	/// <summary>
+	/// Is called when <see cref="PlayerManager.OnPlayerBridgesChanged"/> is invoked. 
+	/// Updates the private network variables of bridges count to sync between players.
+	/// Is only called on Server.
+	/// </summary>
 	private void PlayerManager_OnPlayerBridgesChanged()
 	{
 		_p1Bridges.Value = PlayerManager.player1.BridgesCount;
 		_p2Bridges.Value = PlayerManager.player2.BridgesCount;
 	}
 
+	/// <summary>
+	/// Is called when <see cref="PlayerManager.OnPlayerTurnSwitch"/> is invoked. 
+	/// Updates the private network variables of player turn to sync between players.
+	/// Is only called on Server.
+	/// </summary>
 	private void PlayerManager_OnPlayerTurnSwitch()
 	{
 		_playerDrawingChanged.Value = PlayerManager.playerTurn switch
